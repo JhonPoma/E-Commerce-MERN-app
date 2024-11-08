4cc8cc9995ebf6bf2eb35884d9e2e1240f5304b0
JhonPoma <jpm11797@gmail.com>
2024-11-07 19:58:54 -0500

    centralizamos los endpoints en la carpeta common

---
+++ frontend/common/index.js
@@ -0,0 +1,12 @@
+
+const backendDomain = `http://localhost:8888`
+
+const ResumenApi = {
+    singUP : {
+        url : `${backendDomain}/api/signup`,
+        method : "post"
+    }
+}
+
+
+export default ResumenApi
